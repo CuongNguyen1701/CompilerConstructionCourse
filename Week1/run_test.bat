5011b60ada2e42b861dfecfbea30c67bb524ff0c
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2024-11-19 21:45:21 +0700

    W1: Refactor

---
+++ Week1/run_test.bat
@@ -0,0 +1,6 @@
+start %~dp0\scanner.exe test\example1.kpl > test\example1.out.txt
+start %~dp0\scanner.exe test\example2.kpl > test\example2.out.txt
+start %~dp0\scanner.exe test\example3.kpl > test\example3.out.txt
+start %~dp0\scanner.exe test\example4.kpl > test\example4.out.txt
+
+ 
