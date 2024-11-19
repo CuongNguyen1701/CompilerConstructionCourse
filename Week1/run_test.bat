7db1850a24281431ea872c11e3c823056c438285
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2024-11-19 21:50:37 +0700

    W1: Readd output txt files

--- Week1/run_test.bat
+++ Week1/run_test.bat
@@ -1,6 +1,6 @@
-start %~dp0\scanner.exe test\example1.kpl > test\example1.out.txt
-start %~dp0\scanner.exe test\example2.kpl > test\example2.out.txt
-start %~dp0\scanner.exe test\example3.kpl > test\example3.out.txt
-start %~dp0\scanner.exe test\example4.kpl > test\example4.out.txt
+start /wait cmd /c "%~dp0\scanner.exe test\example1.kpl > %~dp0\test\example1.out.txt"
+start /wait cmd /c "%~dp0\scanner.exe test\example2.kpl > %~dp0\test\example2.out.txt"
+start /wait cmd /c "%~dp0\scanner.exe test\example3.kpl > %~dp0\test\example3.out.txt"
+start /wait cmd /c "%~dp0\scanner.exe test\example4.kpl > %~dp0\test\example4.out.txt"
 
  
